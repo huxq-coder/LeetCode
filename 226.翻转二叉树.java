53c1216c367253a6a1604ef2e6bfb47d27bc268d
胡 <465310135@qq.com>
2020-09-16 09:09:48 +0800

    add 226.翻转二叉树.java

---
+++ 226.翻转二叉树.java
@@ -0,0 +1,37 @@
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=226 lang=java
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    /**
+     * 递归
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode leftNode = invertTree(root.left);
+        TreeNode rightNode = invertTree(root.right);
+        root.left = rightNode;
+        root.right = leftNode;
+        return root;
+    }
+}
+// @lc code=end
+
